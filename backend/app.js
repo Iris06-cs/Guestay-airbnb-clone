7a1e6765f4f6b5b058574defe4ac79107cea8457
IrisW <100994372+Iris06-cs@users.noreply.github.com>
2022-12-13 19:57:56 -0800

    install packages and initial setups

---
+++ backend/app.js
@@ -0,0 +1,49 @@
+//import packages
+const express = require("express");
+require("express-async-errors");
+const morgan = require("morgan");
+const cors = require("cors");
+const csurf = require("csurf");
+const helmet = require("helmet");
+const cookieParser = require("cookie-parser");
+
+const routes = require("./routes");
+
+const { environment } = require("./config");
+const isProduction = environment === "production";
+
+//initialize express application
+const app = express();
+//connect morgan logger middleware
+app.use(morgan("dev"));
+//cookie-parser middleware
+app.use(cookieParser());
+app.use(express.json());
+
+// Security Middleware
+if (!isProduction) {
+  // enable cors only in development
+  app.use(cors());
+}
+
+// helmet helps set a variety of headers to better secure your app
+app.use(
+  helmet.crossOriginResourcePolicy({
+    policy: "cross-origin",
+  })
+);
+
+// Set the _csrf token and create req.csrfToken method
+app.use(
+  csurf({
+    cookie: {
+      secure: isProduction,
+      sameSite: isProduction && "Lax",
+      httpOnly: true,
+    },
+  })
+);
+//connect routes
+app.use(routes);
+
+module.exports = app;
