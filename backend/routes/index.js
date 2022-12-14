82431efa5a29233839cfda0987277b0f412ce830
IrisW <100994372+Iris06-cs@users.noreply.github.com>
2022-12-13 20:06:04 -0800

    Initialize Express and Sequelize with CSRF protection

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -8,4 +8,13 @@ const router = express.Router();
 //   res.send("Hello World!");
 // });
 
+// Add a XSRF-TOKEN cookie
+router.get("/api/csrf/restore", (req, res) => {
+  const csrfToken = req.csrfToken();
+  res.cookie("XSRF-TOKEN", csrfToken);
+  res.status(200).json({
+    "XSRF-Token": csrfToken,
+  });
+});
+
 module.exports = router;
