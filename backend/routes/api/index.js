d6cfb2c0f0f129a285a79e478502386a880abbad
IrisW <100994372+Iris06-cs@users.noreply.github.com>
2022-12-17 14:11:57 -0800

    delete testing route

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -4,32 +4,6 @@ const { restoreUser } = require("../../utils/auth");
 const sessionRouter = require("./session.js");
 const usersRouter = require("./users.js");
 
-//testing auth middleware route
-// // GET /api/set-token-cookie
-// const { setTokenCookie } = require("../../utils/auth.js");
-// const { User } = require("../../db/models");
-// router.get("/set-token-cookie", async (_req, res) => {
-//   const user = await User.findOne({
-//     where: {
-//       username: "FakeUser1",
-//     },
-//   });
-//   setTokenCookie(res, user);
-//   return res.json({ user: user });
-// });
-// // GET /api/restore-user
-// const { restoreUser } = require("../../utils/auth.js");
-// router.use(restoreUser);
-// router.get("/restore-user", (req, res) => {
-//   return res.json(req.user);
-// });
-// // GET /api/require-auth
-// const { requireAuth } = require("../../utils/auth.js");
-// router.get("/require-auth", requireAuth, (req, res) => {
-//   return res.json(req.user);
-// });
-//test api route
-
 //connect to restoreUser middleware
 // If current user session is valid, set req.user to the user in the database
 // If current user session is not valid, set req.user to null
