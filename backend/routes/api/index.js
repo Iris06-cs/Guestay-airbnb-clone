b0442379610888ddef65cd0890be04c195a3e62d
IrisW <100994372+Iris06-cs@users.noreply.github.com>
2022-12-19 00:40:00 -0800

    add get all spots endpoint

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -3,6 +3,7 @@ const router = require("express").Router();
 const { restoreUser } = require("../../utils/auth");
 const sessionRouter = require("./session.js");
 const usersRouter = require("./users.js");
+const spotsRouter = require("./spots.js");
 
 //connect to restoreUser middleware
 // If current user session is valid, set req.user to the user in the database
@@ -12,6 +13,9 @@ router.use(restoreUser);
 router.use("/session", sessionRouter);
 //connect to userRouter
 router.use("/users", usersRouter);
+//connect to spotsRouter
+router.use("/spots", spotsRouter);
+
 router.post("/test", function (req, res) {
   res.json({ requestBody: req.body });
 });
---
+++ backend/routes/api/spots.js
@@ -0,0 +1,38 @@
+// backend/routes/api/spots.js
+const express = require("express");
+const router = express.Router();
+const { restoreUser, requireAuth } = require("../../utils/auth");
+const { Spot, User, Review, SpotImage, sequelize } = require("../../db/models");
+const { check } = require("express-validator");
+const { handleValidationErrors } = require("../../utils/validation");
+const { Op } = require("sequelize");
+//Get all spots owned by the current user
+
+//Get all spots
+router.get("/", async (req, res, next) => {
+  const spots = [];
+  const spotsData = await Spot.findAll();
+  for (let spot of spotsData) {
+    const reviewData = await spot.getReviews({
+      attributes: {
+        include: [[sequelize.fn("AVG", sequelize.col("stars")), "avgRating"]],
+      },
+    });
+    const previewImage = await SpotImage.findOne({
+      attributes: ["url"],
+      where: {
+        spotId: spot.id,
+        preview: true,
+      },
+    });
+    const spotData = spot.toJSON();
+    spotData.avgRating = reviewData[0].toJSON().avgRating;
+    spotData.previewImage = previewImage.url;
+    spots.push(spotData);
+  }
+  res.json({ spots });
+});
+
+module.exports = router;
+
+// SELECT `Spot`.`id`, `Spot`.`ownerId`, `Spot`.`address`, `Spot`.`city`, `Spot`.`state`, `Spot`.`country`, `Spot`.`lat`, `Spot`.`lng`, `Spot`.`name`, `Spot`.`description`, `Spot`.`price`, `Spot`.`createdAt`, `Spot`.`updatedAt`, `Reviews`.`id` AS `Reviews.id`, AVG(`stars`) AS `Reviews.avgRating` FROM `Spots` AS `Spot` LEFT OUTER JOIN `Reviews` AS `Reviews` ON `Spot`.`id` = `Reviews`.`spotId`;
