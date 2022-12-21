ecfe2b142f6dbbfbb8882264e8fc14ef18161239
IrisW <100994372+Iris06-cs@users.noreply.github.com>
2022-12-21 15:17:18 -0800

    create add image to review endpoint

--- backend/db/models/reviewimage.js
+++ backend/db/models/reviewimage.js
@@ -20,7 +20,7 @@ module.exports = (sequelize, DataTypes) => {
       url: {
         type: DataTypes.STRING,
         allowNull: false,
-        unique: true,
+        // unique: true,
         validate: {
           isUrl: true,
         },
--- backend/routes/api/reviews.js
+++ backend/routes/api/reviews.js
@@ -15,7 +15,56 @@ const { Op } = require("sequelize");
 const spot = require("../../db/models/spot");
 const review = require("../../db/models/review");
 const { dateFormat } = require("../../utils/dataFormatter");
-
+//--------------Add an image to a review based on the review id
+const validateReqBody = [
+  check("url").exists({ checkFalsy: true }).withMessage("url is required"),
+  handleValidationErrors,
+];
+router.post(
+  "/:reviewId/images",
+  requireAuth,
+  validateReqBody,
+  async (req, res, next) => {
+    const { reviewId } = req.params;
+    const { url } = req.body;
+    const currentUserId = req.user.id;
+    //check if review exits
+    const review = await Review.findByPk(reviewId);
+    if (review) {
+      //check if review belongs to current user
+      if (review.userId === currentUserId) {
+        const images = await review.getReviewImages();
+        if (images.length === 10) {
+          const err = new Error(
+            "Maximum number of images for this resource was reached"
+          );
+          err.status = 403;
+          err.title = "Maximum number of images for this resource was reached";
+          err.errors = [
+            "Maximum number of images for this resource was reached",
+          ];
+          return next(err);
+        } else {
+          const newImage = await review.createReviewImage({ url });
+          let resObj = {};
+          resObj.id = newImage.id;
+          resObj.url = newImage.url;
+          return res.json(resObj);
+        }
+      } else {
+        const err = new Error("Forbidden");
+        err.status = 403;
+        next(err);
+      }
+    } else {
+      const err = new Error("Review couldn't be found");
+      err.status = 404;
+      err.title = "Review couldn't be found";
+      err.errors = ["Review couldn't be found with the provided review id"];
+      return next(err);
+    }
+  }
+);
 //--------------Get all Reviews of the current user
 router.get("/current", requireAuth, async (req, res, next) => {
   const userId = req.user.id;
