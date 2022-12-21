495a9e4788ecd940997ccd4b7fc27fc425621373
IrisW <100994372+Iris06-cs@users.noreply.github.com>
2022-12-20 21:58:29 -0800

    modify index file

--- backend/db/migrations/20221221054711-add-lat-lng-indexes.js
+++ backend/db/migrations/20221221054711-add-lat-lng-indexes.js
@@ -1,11 +1,19 @@
 "use strict";
-
+let options = {};
+if (process.env.NODE_ENV === "production") {
+  options.schema = process.env.SCHEMA; // define your schema in options object
+}
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.addIndex("Spots", ["lat", "lng"], {
-      unique: true,
-    });
+    await queryInterface.addIndex(
+      "Spots",
+      ["lat", "lng"],
+      {
+        unique: true,
+      },
+      options
+    );
   },
 
   async down(queryInterface, Sequelize) {
