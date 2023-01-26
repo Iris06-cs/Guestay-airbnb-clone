0e2af8ea0887005777dc10503c5651f28be786d5
IrisW <100994372+Iris06-cs@users.noreply.github.com>
2023-01-25 20:10:52 -0800

    development frontend xsrf-token setup

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -8,15 +8,21 @@ const router = express.Router();
 //   res.send("Hello World!");
 // });
 
-// Add a XSRF-TOKEN cookie
-router.get("/api/csrf/restore", (req, res) => {
-  const csrfToken = req.csrfToken();
-  res.cookie("XSRF-TOKEN", csrfToken);
-  res.status(200).json({
-    "XSRF-Token": csrfToken,
+// // Add a XSRF-TOKEN cookie
+// router.get("/api/csrf/restore", (req, res) => {
+//   const csrfToken = req.csrfToken();
+//   res.cookie("XSRF-TOKEN", csrfToken);
+//   res.status(200).json({
+//     "XSRF-Token": csrfToken,
+//   });
+// });
+// Add a XSRF-TOKEN cookie in development
+if (process.env.NODE_ENV !== "production") {
+  router.get("/api/csrf/restore", (req, res) => {
+    res.cookie("XSRF-TOKEN", req.csrfToken());
+    return res.json({});
   });
-});
-
+}
 //connect apiRouter to router
 const apiRouter = require("./api");
 router.use("/api", apiRouter);
